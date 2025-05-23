d59536b941e30f11bfee747e78fc31fcdf055633
Geraaaaaaa <gerardocarrizosavalentin1a@gmail.com>
2025-05-22 18:52:52 -0600

    feat(backend): Add CORS for frontend requests

--- server/config/env.js
+++ server/config/env.js
@@ -11,7 +11,8 @@ const envConfig = {
     db_user: process.env.DB_USER,
     db_password: process.env.DB_PASSWORD,
     db_port: process.env.DB_PORT,
-    db_database: process.env.DB_DATABASE
+    db_database: process.env.DB_DATABASE,
+    client_url: process.env.CLIENT_URL
 }
 
 export default envConfig
--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 import express, { json } from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import taskRouter from './routes/taskRoute.js'
 import userRouter from './routes/userRoute.js'
+import envConfig from './config/env.js'
+
 
 const app = express()
 app.use(morgan('dev'))
 app.use(json())
+
+const clientUrl = envConfig.client_url
+
+const corsOptions = {
+    origin : [`${clientUrl}`],
+    methods : ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders : ["Content-type", 'Authorization'],
+    optionSuccessStatus : 200,
+    credentials : true
+}
+
+app.use(cors(corsOptions))
 app.use('/tasks', taskRouter)
 app.use('/users', userRouter)
 
